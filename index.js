559702eaf416b7cb23017af8502e78b44b548652
1072030 <229jdu@gmail.com>
2021-09-17 22:32:09 +0800

    test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const line = require("@line/bot-sdk");
 const express = require("express");
 const app = express();
+const handleEvent = require("../hook/handleEvent");
+const config = require("./config/lineConfig");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get("/", async (_, res) => {
@@ -9,8 +11,17 @@ app.get("/", async (_, res) => {
     message: "Connected successfully!",
   });
 });
-const handleEventRouter = require("./routes/webhook");
-app.use("/", handleEventRouter);
+
+app.post("/callback", line.middleware(config), (req, res) => {
+  console.log(req);
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.log("error", err);
+      res.status(500).end();
+    });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
