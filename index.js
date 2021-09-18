3448772903125277e599278e8edcf268a8e9ce70
1072030 <229jdu@gmail.com>
2021-09-18 19:27:59 +0800

    ts

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const line = require("@line/bot-sdk");
 const express = require("express");
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 const config = {
   channelSecret: "b85d02c4583b0a223741ee0ea2e28c7c",
   channelAccessToken:
     "ZQkIGNlpA/3kmzloMpBXRCVla9qW7mUYpiN3ZcjYCBQolzbjYq60zKIOgoZtTNNM5IGIPG1kH4DxyxmugnM6QlJF1W4LmV+9osHEox2G1FDPKVWxIosj1o4VP8cKkdxf0gXo6bBjulEVnsAmlIDqZQdB04t89/1O/w1cDnyilFU=",
 };
 const client = new line.Client(config);
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", async (_, res) => {
   return res.status(200).json({
