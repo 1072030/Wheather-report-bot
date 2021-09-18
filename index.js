8fda5d725fc14652e742bc1b1655e6c374691d80
1072030 <229jdu@gmail.com>
2021-09-18 18:10:26 +0800

    T

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const line = require("@line/bot-sdk");
 const express = require("express");
 const app = express();
-const handleEvent = require("./hook/handleEvent");
 const config = require("./config/lineConfig");
+const handleText = require("./hook/handleText");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get("/", async (_, res) => {
@@ -26,3 +26,22 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 }); //
+
+const handleEvent = async (event) => {
+  switch (event.type) {
+    case "message":
+      const message = event.message;
+      switch (message.type) {
+        case "text":
+          return handleText(message, event.replyToken, event.source);
+        // case "image":
+        //   return handleImage(message, event.replyToken);
+        // case "location":
+        // return handleLocation(message, event.replyToken);
+        // default:
+        //   throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+      }
+    default:
+      throw new Error(`Unknown event: ${JSON.stringify(event)}`);
+  }
+};
