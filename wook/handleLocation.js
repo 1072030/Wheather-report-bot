b305d7cc81649d947739387a917048983cf44a4c
1072030 <229jdu@gmail.com>
2021-09-19 16:04:55 +0800

    ts

--- wook/handleLocation.js
+++ wook/handleLocation.js
@@ -7,7 +7,7 @@ const handleLocation = async (message, replyToken) => {
   console.log(LocationName[0]["Location"]);
   let confirmLocation;
   for (let i = 0, j = LocationName[0]["Location"].length; i < j; i++) {
-    if (message.address.indexOf(LocationName[0]["Location"][i])) {
+    if (message.address.indexOf(LocationName[0]["Location"][i]) != -1) {
       confirmLocation = LocationName[0]["Location"][i];
     }
   }
