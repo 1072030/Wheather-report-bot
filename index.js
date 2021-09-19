92ad28ba9878d9e74ad90ec0e7c07e236c0129b5
1072030 <229jdu@gmail.com>
2021-09-19 12:22:56 +0800

    ts

--- index.js
+++ index.js
@@ -62,8 +62,6 @@ function handleText(message, replyToken, source) {
       title: "My button sample",
       text: "Hello, my button",
       actions: [
-        { label: "Go to line.me", type: "uri", uri: "https://line.me" },
-        { label: "Say hello1", type: "postback", data: "hello こんにちは" },
         {
           label: "言 hello2",
           type: "postback",
