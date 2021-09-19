cdd2332c51fb7e572ccfcffea60f0d8378b84122
1072030 <229jdu@gmail.com>
2021-09-19 15:44:40 +0800

    ts

--- wook/handleLocation.js
+++ wook/handleLocation.js
@@ -1,8 +1,17 @@
+const client = require("../config/client");
 const fetchWeather = require("./fetchRequire");
 
 const handleLocation = async (message, replyToken) => {
+  console.log(message);
   const LocationName = await fetchWeather();
   console.log(LocationName[0]["Location"]);
+  let confirmLocation;
+  /* if (LocationName.includes(message.text)) {
+  } */
+  return await client.replyMessage(replyToken, {
+    type: "text",
+    text: "test",
+  });
 };
 
 module.exports = handleLocation;
