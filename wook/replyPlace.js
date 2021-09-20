4b10be91c84906007e144315b0a23218ff91fcd8
1072030 <229jdu@gmail.com>
2021-09-20 11:49:27 +0800

    ts

--- wook/replyPlace.js
+++ wook/replyPlace.js
@@ -3,35 +3,26 @@ const fetchWeather = require("./fetchRequire");
 
 const replyPlace = async (Place, replyToken) => {
   const LocationName = await fetchWeather();
-  console.log(LocationName[0].Location);
-  console.log(LocationName[1]);
-  /* console.log(LocationName[0]);
-  console.log(LocationName[0]);
-  console.log(LocationName[0]); */
+  console.log(LocationName);
   let confirmLocation = [];
 
   for (let i = 0, j = LocationName[0]["Location"].length; i < j; i++) {
     if (Place.indexOf(LocationName[0]["Location"][i]) != -1) {
-      /* console.log(LocationName[0]["Location"][i]); */
-      /* console.log(LocationName[0]); */
-      /* console.log(LocationName[0]["MaxT"][i]);
-      console.log(LocationName[0]["MinT"][i]);
-      console.log(LocationName[0]["Pop"][i]); */
       confirmLocation.push(
         {
           name: LocationName[0]["Location"][i],
         },
         {
-          Weather: LocationName[0]["Weather"][i],
+          Weather: LocationName[1]["Weather"][i],
         },
         {
-          MaxT: LocationName[0]["MaxT"][i],
+          MaxT: LocationName[2]["MaxT"][i],
         },
         {
-          MinT: LocationName[0]["MinT"][i],
+          MinT: LocationName[3]["MinT"][i],
         },
         {
-          Pop: LocationName[0]["Pop"][i],
+          Pop: LocationName[4]["Pop"][i],
         }
       );
     }
