990c4a2687379ce923277fecb7c95b0a41e1df37
1072030 <229jdu@gmail.com>
2021-10-10 16:38:30 +0800

    ts

--- wook/handleBeacon.js
+++ wook/handleBeacon.js
@@ -2,10 +2,10 @@ const client = require("../config/client");
 
 const handleBeacon = async (contents, replyToken) => {
   console.log(contents);
-  await client.replyMessage(replyToken, {
-    type: "text",
-    text: "hello",
-  });
+  // await client.replyMessage(replyToken, {
+  //   type: "text",
+  //   text: "hello",
+  // });
 };
 
 module.exports = handleBeacon;
