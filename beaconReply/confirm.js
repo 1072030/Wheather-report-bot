217f1021f6a84512fe36ec7a3e6c83a50d5e63bc
1072030 <229jdu@gmail.com>
2021-10-10 22:52:34 +0800

    ts

--- beaconReply/confirm.js
+++ beaconReply/confirm.js
@@ -1,11 +1,31 @@
 const firestore = require("../config/firebaseConfig");
 const confirm = async (source, replyToken) => {
   const firestoreData = await firestore.collection("BeaconTest").get();
-  firestoreData.forEach(async (doc) => {});
+  let data = [];
+  firestoreData.forEach(async (doc) => {
+    if (doc.data().BeaconId === parseInt(source.beacon.dm)) {
+      //data.push({});
+    }
+  });
   await client.replyMessage(replyToken, {
-    type: "text",
-    altText: "no",
-    text: "hello",
+    type: "template",
+    altText: "this is a confirm template",
+    template: {
+      type: "confirm",
+      actions: [
+        {
+          type: "uri",
+          label: "是",
+          uri: "https://bootstrap5.hexschool.com/docs/5.0/forms/input-group/",
+        },
+        // {
+        //   type: "message",
+        //   label: "否",
+        //   text: "否",
+        // },
+      ],
+      text: "您確定嗎？",
+    },
   });
 };
 module.exports = confirm;
--- wook/handleBeacon.js
+++ wook/handleBeacon.js
@@ -3,38 +3,23 @@ const firestore = require("../config/firebaseConfig");
 const beaconTypeConfirm = require("../beaconReply/confirm");
 const handleBeacon = async (source, replyToken) => {
   const firestoreData = await firestore.collection("BeaconTest").get();
-  // firestoreData.forEach(async (doc) => {
-  //   if (doc.data().BeaconId === parseInt(source.beacon.dm)) {
-  //     switch (doc.data().type) {
-  //       case "text":
-  //         return;
-  //       case "image":
-  //         return;
-  //       case "confirm":
-  //         return beaconTypeConfirm(source, replyToken);
-  //     }
-  //   }
-  // });
-  await client.replyMessage(replyToken, {
-    type: "template",
-    altText: "this is a confirm template",
-    template: {
-      type: "confirm",
-      actions: [
-        {
-          type: "uri",
-          label: "是",
-          uri: "https://bootstrap5.hexschool.com/docs/5.0/forms/input-group/",
-        },
-        {
-          type: "message",
-          label: "否",
-          text: "否",
-        },
-      ],
-      text: "您確定嗎？",
-    },
+  firestoreData.forEach(async (doc) => {
+    if (doc.data().BeaconId === parseInt(source.beacon.dm)) {
+      switch (doc.data().type) {
+        case "text":
+          return;
+        case "image":
+          return;
+        case "confirm":
+          return beaconTypeConfirm(source, replyToken);
+      }
+    }
   });
+  // await client.replyMessage(replyToken, {
+  //   type: "text",
+  //   altText: "no",
+  //   text: "hello",
+  // });
 };
 
 module.exports = handleBeacon;
