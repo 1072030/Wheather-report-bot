7e7618d44bb08afbed95054110965fe8c16acdb6
1072030 <229jdu@gmail.com>
2021-10-10 23:02:14 +0800

    ts

--- wook/handleBeacon.js
+++ wook/handleBeacon.js
@@ -2,10 +2,11 @@ const client = require("../config/client");
 const firestore = require("../config/firebaseConfig");
 const beaconTypeConfirm = require("../beaconReply/confirm");
 const handleBeacon = async (source, replyToken) => {
+  console.log(source.beacon);
   const firestoreData = await firestore.collection("BeaconTest").get();
   firestoreData.forEach(async (doc) => {
+    console.log(doc.data().type);
     if (doc.data().BeaconId === parseInt(source.beacon.dm)) {
-      console.log(doc.data().type);
       switch (doc.data().type) {
         case "text":
           return;
