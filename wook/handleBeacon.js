dda0ee6577bb45c1bc920a5606c1a2e99b159219
1072030 <229jdu@gmail.com>
2021-10-10 18:17:21 +0800

    ts

--- wook/handleBeacon.js
+++ wook/handleBeacon.js
@@ -1,23 +1,18 @@
 const client = require("../config/client");
 const firestore = require("../config/firebaseConfig");
-const handleBeacon = async (contents, replyToken) => {
+const handleBeacon = async (source, replyToken) => {
   const firestoreData = await firestore.collection("BeaconTest").get();
-  // firestoreData.forEach(async (doc) => {
-  //   if (doc.data().beaconId === source.userId) {
-
-  //     console.log('')
-  //     // await firestore
-  //     //   .collection("BeaconTest")
-  //     //   .doc(doc.id)
-  //     //   .update({ city: data[0] });
-  //   }
-  //   //  else {
-  //   //   await firestore
-  //   //     .collection("User")
-  //   //     .doc(rand)
-  //   //     .set({ userId: source.userId, city: data[0] });
-  //   // }
-  // });
+  firestoreData.forEach(async (doc) => {
+    if (doc.data().beaconId === source.beacon.dm) {
+      console.log("this id is match");
+      // await firestore
+      //   .collection("BeaconTest")
+      //   .doc(doc.id)
+      //   .update({ city: data[0] });
+    } else {
+      console.log("this id doesn't match");
+    }
+  });
   console.log(contents);
   // await client.replyMessage(replyToken, {
   //   type: "text",
