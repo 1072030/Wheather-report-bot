8ecf08fc46f2a2bd49c71326fbfab1baa7c95877
1072030 <46060471+1072030@users.noreply.github.com>
2021-12-03 17:25:13 +0800

    update reply

--- hook/handleBeacon.js
+++ hook/handleBeacon.js
@@ -2,41 +2,37 @@ const client = require("../config/client");
 
 const handleBeacon = async (event, replyToken) => {
   await client.replyMessage(replyToken, {
-    type: "flex",
-    contents: [
-      {
-        type: "carousel",
-        contents: [
-          {
-            type: "template",
-            altText: "this is an image carousel template",
-            template: {
-              type: "image_carousel",
-              columns: [
-                {
-                  imageUrl:
-                    "https://aws-uploade-image-test2.s3.amazonaws.com/0ei9wdytys9324fyqxh2dk.png",
-                  action: {
-                    type: "message",
-                    label: "動作 1",
-                    text: "動作 1",
-                  },
-                },
-                {
-                  imageUrl:
-                    "https://aws-uploade-image-test2.s3.amazonaws.com/0ei9wdytys9324fyqxh2dk.png",
-                  action: {
-                    type: "message",
-                    label: "動作 2",
-                    text: "動作 2",
-                  },
-                },
-              ],
-            },
+    type: "template",
+    altText: "this is a image carousel template",
+    template: {
+      type: "image_carousel",
+      columns: [
+        {
+          imageUrl: "https://example.com/bot/images/item1.jpg",
+          action: {
+            type: "postback",
+            label: "Buy",
+            data: "action=buy&itemid=111",
           },
-        ],
-      },
-    ],
+        },
+        {
+          imageUrl: "https://example.com/bot/images/item2.jpg",
+          action: {
+            type: "message",
+            label: "Yes",
+            text: "yes",
+          },
+        },
+        {
+          imageUrl: "https://example.com/bot/images/item3.jpg",
+          action: {
+            type: "uri",
+            label: "View detail",
+            uri: "http://example.com/page/222",
+          },
+        },
+      ],
+    },
   });
 };
 
